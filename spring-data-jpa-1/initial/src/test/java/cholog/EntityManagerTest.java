407541a4a537f34ec227c8790c2fa2ba6006902a
Choo <choo000407@naver.com>
2024-05-14 15:53:17 +0900

    jpa 엔티티 매핑 학습

--- spring-data-jpa-1/initial/src/test/java/cholog/EntityManagerTest.java
+++ spring-data-jpa-1/initial/src/test/java/cholog/EntityManagerTest.java
@@ -1,13 +1,13 @@
 package cholog;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.jdbc.core.JdbcTemplate;
-
-import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
 public class EntityManagerTest {
--- spring-data-jpa-1/initial/src/test/java/cholog/QueryCreationTest.java
+++ spring-data-jpa-1/initial/src/test/java/cholog/QueryCreationTest.java
@@ -1,14 +1,13 @@
 package cholog;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
+import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-
-import java.util.List;
-
-import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
 public class QueryCreationTest {
